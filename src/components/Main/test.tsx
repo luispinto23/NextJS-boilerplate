ce918e1b23c009d1be4a76916f63a363ddc7c440
Luís Pinto <luispinto23@gmail.com>
2021-05-02 11:25:55 +0100

    Boilerplate configured with Storybook and PWA.

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,10 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot();
   });
+
+  it('should render color correctly', function () {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
+  });
 });
